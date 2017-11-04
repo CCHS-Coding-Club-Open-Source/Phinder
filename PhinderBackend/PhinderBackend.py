c60224a4813c14fe0e9ba839e48e5b721750b12b
nsavory20 <32076173+nsavory20@users.noreply.github.com>
2017-11-04 11:40:26 -0400

    Update PhinderBackend.py

--- PhinderBackend/PhinderBackend.py
+++ PhinderBackend/PhinderBackend.py
@@ -9,3 +9,10 @@ accel = float(input("Enter device's accelerometer reading\n"))
 print("Hello world!")
 
 print(barPressure, time, heading, accel)
+
+
+#Ordering of datatypes:
+#1: Time (Compare to current world times to find phone's timezone.)
+#2 Movement (Compass + directional accelerometer to determine if the phone is moving and in what direction)
+#3 Wi-fi location (Find list of wifi signals meeting the pre-determined criteria
+#4 Barometer (Match those wi-fi signals to the given height of the phone.)
