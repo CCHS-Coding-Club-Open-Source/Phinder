5cb579f68928e3e7f3719d80493e40c9b562faf3
UnicodeUnicorn <UnicodeUnicorn@users.noreply.github.com>
2017-11-04 11:01:06 -0400

    Clarify inputs

--- PhinderBackend/PhinderBackend.py
+++ PhinderBackend/PhinderBackend.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 barPressure = float(input("Enter barometric pressure:\n"))
-time = input("Enter device time as 'min:sec'\n").split(':')
+time = input("Enter device time as 'min:sec'\n").split(':') #time zones
 heading = float(input("Enter device's compass heading:\n"))
-currentSpeed = float(input("Enter device's current speed\n"))
+accel = float(input("Enter device's accelerometer reading\n"))
 
 
 print("Hello world!")
 
-print(barPressure, time, heading, currentSpeed)
+print(barPressure, time, heading, accel)
