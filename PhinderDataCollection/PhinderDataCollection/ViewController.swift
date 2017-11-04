b8294b266287ca18b3a6b426c2054a87f6d6c6e1
alset333 <alset333@icloud.com>
2017-11-04 10:45:26 -0400

    Xcode 1st commit

---
+++ PhinderDataCollection/PhinderDataCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhinderDataCollection
+//
+//  Created by Peter Maar on 11/4/17.
+//  Copyright © 2017 alset333. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
