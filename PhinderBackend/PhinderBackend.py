84f90e3bf444151497983397db1ed82f30328848
alset333 <alset333@icloud.com>
2017-11-04 10:23:54 -0400

    Example get and print data

--- PhinderBackend/PhinderBackend.py
+++ PhinderBackend/PhinderBackend.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 
+barPressure = input("Enter barometric pressure:\n")
+deviceTime = input("Enter device time:\n")
+heading = input("Enter device's compass heading")
+currentSpeed = input("Enter device's current speed\n")
+
+
 print("Hello world!")
+
+print(barPressure, deviceTime, heading, currentSpeed)
